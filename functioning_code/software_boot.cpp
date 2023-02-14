50bf23a250b6fd3528c718dfa0f403cf71b46ae6
Tyler Chu <88418068+tyler-chu@users.noreply.github.com>
2023-02-14 14:39:49 -0800

    Update software_boot.cpp

--- functioning_code/software_boot.cpp
+++ functioning_code/software_boot.cpp
@@ -1,26 +1,56 @@
 /**
  * @file software_boot.txt
  * @author Tyler, Joey
- * @brief Uses software to boot up the BMS IC. Uses Pin #21 (any GPIO pin) and sets it to HIGH (will turn on BMS IC).
- *        No need to use physical boot button anymore.
+ * @brief Uses software to boot up the BMS IC. Uses Pin #3 (any GPIO pin) and sets it to HIGH (will turn on BMS IC).
+ *        No need to use physical boot button anymore. Uses i2c validation to check if successfully turned on
+ *        (if i2c bus = created, BMS = ON, if i2c bus != created, BMS = OFF).
  */
 
 #include <Wire.h>
 #include <Arduino.h>
 
+#define I2C_SDA 7
+#define I2C_SCL 9
+#define BOOT 3
+#define SLAVE_ADDRESS 0x18
+
+bool boot_flag = false;
+
 void setup()
 {
-  delay(1000);
-  pinMode(21, OUTPUT);
+  Wire.begin(I2C_SDA, I2C_SCL);
   Serial.begin(115200);
-  Serial.print("Booting Up from Setup...");
-  delay(1000);
 
-  digitalWrite(21, HIGH);  // switch diode on
-  Serial.println("LED turned on...");
-  delay(3); // 3 ms = 3000 us 
-  digitalWrite(21, LOW);
 }
 
 void loop()
-{}
+{
+  // Boot up BMS w/ Software
+  while (!boot_flag) {
+    delay(1000);
+    pinMode(BOOT, OUTPUT);
+    // Serial.begin(115200);
+    Serial.println("Booting Up from Setup...");
+    delay(1000);
+
+    digitalWrite(BOOT, HIGH);  // switch diode on
+    Serial.println("LED turned on...");
+    delay(3); // 3 ms = 3000 us 
+    digitalWrite(BOOT, LOW);
+
+    Wire.beginTransmission(SLAVE_ADDRESS);
+    byte error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.println("BMS IC has successfully booted up.");
+      boot_flag = true;
+    }
+    else
+      Serial.println("BMS IC: OFF.\n");
+
+    delay(2000);
+
+  }
+  
+
+}
