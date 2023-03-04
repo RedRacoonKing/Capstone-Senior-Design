98b085b1d4ddcc25a424252ee729ba55cbe3748c
Tyler Chu <88418068+tyler-chu@users.noreply.github.com>
2023-03-03 16:19:31 -0800

    3/3: main.cpp

--- functioning_code/main.cpp
+++ functioning_code/main.cpp
@@ -9,14 +9,23 @@
 #include <LED_Config.h>
 #include <I2C_Config.h>
 #include <Boot_Config.h>
+#include <SPI_Config.h>
+#include <LCD_Config.h>
 
 void setup()
 {
   i2c_setup();
-  i2c_scanner();
-  // led_setup();
-  // boot_bms();
-  // switch_setup();
+  // i2c_scanner();
+  led_setup();
+  boot_bms();
+  switch_setup();
+
+  i2c_write();
+
+  // spi_setup();
+  // spi_write();
+  lcd_setup();
+  lcd_display_ui();
 }
 
 void loop()
@@ -26,6 +35,5 @@ void loop()
   if (button.isPressed()){
     led_logging();
   }
-  // i2c read/write here
 }
 
