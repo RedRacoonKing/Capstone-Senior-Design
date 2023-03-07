ed9f9cc2c2d6d78011fd188ca10b3c5c56d5fa68
Tyler Chu <88418068+tyler-chu@users.noreply.github.com>
2023-03-06 16:12:38 -0800

    3/6/23: main.cpp

--- functioning_code/main.cpp
+++ functioning_code/main.cpp
@@ -7,33 +7,41 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include <LED_Config.h>
-#include <I2C_Config.h>
+#include <BMS_Config.h>
 #include <Boot_Config.h>
-#include <SPI_Config.h>
+#include <SD_Config.h>
 #include <LCD_Config.h>
 
 void setup()
 {
+  delay(3000);
   i2c_setup();
+  // Serial.begin(115200);
+  // delay(1000);
   // i2c_scanner();
   led_setup();
   boot_bms();
+  bms_setup();
   switch_setup();
 
-  i2c_write();
+  // i2c_write();
 
-  // spi_setup();
+  spi_setup();
+  // spi_write_test();
   // spi_write();
-  lcd_setup();
-  lcd_display_ui();
+  // lcd_setup();
+  // lcd_display_ui();
+  // clear_sys_stat();
+  // test_sys_ctrl1();
 }
 
 void loop()
 {
-  button.loop();
+  led_logging();
+  // button.loop();
 
-  if (button.isPressed()){
-    led_logging();
-  }
+  // if (button.isPressed()){
+  //   led_logging();
+  // }
 }
 
